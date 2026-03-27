044119ce84507886888458affcc8bf42d2f30e95
Atharv <atharvjoshi1077@gmail.com>
2026-03-27 12:16:01 +0530

    Companies and Alumni component created

--- College_Website/src/App.jsx
+++ College_Website/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./layout/Navbar";
+
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="bg-white">
+        <Navbar />
+      </div>
     </>
   );
 }
