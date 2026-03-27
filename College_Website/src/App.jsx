a1a4c4033b9065dcedf466180be114624adb2671
zameernagaral <zameercodes@gmail.com>
2026-03-27 11:37:46 +0530

    Add routing with react-router-dom and update Navbar for navigation

--- College_Website/src/App.jsx
+++ College_Website/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Navbar from "./layout/Navbar";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+    </BrowserRouter>
   );
 }
 
