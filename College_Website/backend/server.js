01edbf93c27806a84cc5d9a8fb521119338abbf5
zameernagaral <zameercodes@gmail.com>
2026-03-27 16:51:44 +0530

    vercel deploy

--- College_Website/backend/server.js
+++ College_Website/backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 // Middleware
 app.use(cors());
 app.use(express.json());
